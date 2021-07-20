e48941189375eebc25c1f3c418f549e67b2b5c76
Md-Imam-Hasan <imamhasan0007@gmail.com>
2021-07-20 20:38:09 +0600

    jwt cookie authentication

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -4,10 +4,14 @@ const express = require("express");
 // internal imports
 const { getInbox } = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { checkLogin } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
+// set page title
+const page_title = "Inbox";
+
 // Load inbox page
-router.get("/", decorateHtmlResponse("Inbox"), getInbox);
+router.get("/", decorateHtmlResponse(page_title), checkLogin, getInbox);
 
 module.exports = router;
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,12 +2,31 @@
 const express = require("express");
 
 // internal imports
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login, logout } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  doLoginValidator,
+  doLoginValidationHandler,
+} = require("../middlewares/login/loginValidators");
+const { redirectLoggedIn } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
+// set the page title
+const page_title = "Login";
+
 // Load login page
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(page_title), redirectLoggedIn, getLogin);
+
+// login
+router.post(
+  "/",
+  decorateHtmlResponse(page_title),
+  doLoginValidator,
+  doLoginValidationHandler,
+  login
+);
+
+router.delete("/", logout);
 
 module.exports = router;
